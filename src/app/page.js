87f3221c449430b75850ff0575596c4601755197
dark-mirage <username@users.noreply.github.com>
2025-08-14 15:48:04 +0500

    ready structure

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <div className="min-h-screen flex items-center justify-center bg-gray-900">
+    <h1 className="text-white text-3xl font-bold">Tailwind работает!</h1>
+  </div>
+  );
+}
