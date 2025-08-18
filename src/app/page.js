ea6a28f3c0899d696bb969d5957d96050af731c0
dark-mirage <username@users.noreply.github.com>
2025-08-18 13:53:27 +0500

    testing

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import ProfileClient from "./profile/ProfileClient"; // путь правильный
+
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/login");
+  }
+
+  return <ProfileClient />; 
+}
